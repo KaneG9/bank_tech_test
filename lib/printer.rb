8e20781559b218176731eed38037715f91715ab4
Kane <kgin888@gmail.com>
2021-06-02 18:19:49 +0100

    refactoring view_statement

--- lib/printer.rb
+++ lib/printer.rb
@@ -2,14 +2,12 @@
 
 class Printer
   def self.view_statement(transaction_log)
-    balance = 0
-    statement = ''
+    balance, statement = 0, ''
     transaction_log.each do |transaction|
       balance += (transaction.credit - transaction.debit)
       statement = "#{transaction.date} || #{format_data(transaction.credit)} || #{format_data(transaction.debit)} || #{format_data(balance)}\n" + statement
     end
-    statement = "date || credit || debit || balance\n#{statement}"
-    statement.strip
+    statement = "date || credit || debit || balance\n#{statement}".strip
   end
 
   def self.format_data(data)
